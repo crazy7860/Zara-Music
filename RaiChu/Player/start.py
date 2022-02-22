71844326ab7e22689e46eb8498ace569f7d9ae06
It's Hussain <90657846+The-Death-Soul@users.noreply.github.com>
2022-02-23 01:16:46 +0530

    Update start.py

--- RaiChu/Player/start.py
+++ RaiChu/Player/start.py
@@ -33,29 +33,29 @@ async def _human_time_duration(seconds):
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
         await message.reply_text(
-        f"""**I ᴀᴍ 𝘽𝙤𝙩 𝘿𝙪𝙣𝙞𝙮𝙖 𝙈𝙪𝙨𝙞𝙘   
-ʙᴏᴛ ʜᴀɴᴅʟᴇ ʙʏ [𝘽𝙤𝙩 𝘿𝙪𝙣𝙞𝙮𝙖](https://t.me/BotDuniya)
-Thanks to add me 😇**
+        f"""**I ᴀᴍ 𝒁𝒂𝒓𝒂 𝑽𝒄 𝑷𝒍𝒂𝒚𝒆𝒓   
+ʙᴏᴛ 𝙾𝚆𝙽𝙴𝙳 ʙʏ [𝑯𝒖𝒔𝒔𝒂𝒊𝒏](https://t.me/The_Death_Soul)
+Thanks to add me 😜**
         """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "Handle", url="https://t.me/FriDayNetwork"
+                        "🍹𝐎𝐰𝐧𝐞𝐫🍹", url="https://t.me/The_Death_Soul"
                     ),
                     InlineKeyboardButton(
-                        "𝐂𝐨𝐦𝐦𝐚𝐧𝐝 𝐋𝐢𝐬𝐭🧰", url="https://telegra.ph/%F0%9D%98%BE%F0%9D%99%A4%F0%9D%99%A2%F0%9D%99%A2%F0%9D%99%96%F0%9D%99%A3%F0%9D%99%99%F0%9D%99%A8-%F0%9D%99%81%F0%9D%99%A4%F0%9D%99%A7-%F0%9D%98%BD%F0%9D%99%A4%F0%9D%99%A9-%F0%9D%98%BF%F0%9D%99%AA%F0%9D%99%A3%F0%9D%99%9E%F0%9D%99%AE%F0%9D%99%96-%F0%9D%98%BD%F0%9D%99%8A%F0%9D%99%8F-02-08-3"
+                        "🛡️𝐅𝐞𝐝𝐞𝐫𝐚𝐭𝐢𝐨𝐧🛡️", url="https://t.me/Death_Soul_Federation"
                     )
                   ],[
                     InlineKeyboardButton(
-                       " 𝐒𝐮𝐩𝐩𝐨𝐫𝐭👿", url="https://t.me/PmPermit"
+                       "🥂𝐒𝐮𝐩𝐩𝐨𝐫𝐭🥂", url="https://t.me/Love_Dear_Comrades"
                     ),
                     InlineKeyboardButton(
-                        "𝐔𝐩𝐝𝐚𝐭𝐞𝐬", url="https://t.me/BotDuniya"
+                        "🍸𝐔𝐩𝐝𝐚𝐭𝐞𝐬🍸", url="https://t.me/ZaraSupport"
                     )
                 ],[
                     InlineKeyboardButton(
-                        "➕ 𝐀𝐝𝐝 𝐌𝐞 𝐓𝐨 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩➕",
+                        "➕𝐀𝐝𝐝 𝐌𝐞 𝐓𝐨 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩➕",
                         url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
                     )
                 ]
